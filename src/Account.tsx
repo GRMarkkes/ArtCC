d645c2c8c6bfb1aa1229e38207813efe522d5d9c
UsmanZulfiqar <apple@apples-MacBook-Pro.local>
2023-03-22 12:22:25 +0500

    first commit

--- src/Account.tsx
+++ src/Account.tsx
@@ -17,37 +17,23 @@ const Account: React.FC<Props> = ({ setSubmit, wallet }: any) => {
   );
   const [amount, setAmount] = useState("");
 
-  // var sourceKeys = StellarSdk.Keypair.fromSecret(
-  //   "SCJ4DETZH4P6ESXA5DAWE7L5EJHI2F3ZCW5RJ4YSQK2CNTIL3FZX3R4Z"
-  // );
-  // var destinationId =
-  //   "GCDTOO2NWAGR4JIT57HVKY4GOCHMFWNWKLHQG2TU6S77SNFWWEBSEYOM";
-  // // Transaction will hold a built transaction we can resubmit if the result is unknown.
-
   const handler = () => {
     setLoading(true);
     var server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
     var sourceKeys = StellarSdk.Keypair.fromSecret(accountAddress);
-    var destinationId = destinationAddress; // Transaction will hold a built transaction we can resubmit if the result is unknown.
-    var transaction;
+    var destinationId = destinationAddress;
 
-    // First, check to make sure that the destination account exists.
-    // You could skip this, but if the account does not exist, you will be charged
-    // the transaction fee when the transaction fails.
     server
       .loadAccount(destinationId)
-      // If the account is not found, surface a nicer error message for logging.
       .catch(function (error: any) {
         if (error instanceof StellarSdk.NotFoundError) {
           throw new Error("The destination account does not exist!");
         } else return error;
       })
-      // If there was no error, load up-to-date information on your account.
       .then(function () {
         return server.loadAccount(sourceKeys.publicKey());
       })
       .then(function (sourceAccount: any) {
-        // Start building the transaction.
         transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
           fee: StellarSdk.BASE_FEE,
           networkPassphrase: StellarSdk.Networks.TESTNET,
@@ -55,21 +41,14 @@ const Account: React.FC<Props> = ({ setSubmit, wallet }: any) => {
           .addOperation(
             StellarSdk.Operation.payment({
               destination: destinationId,
-              // Because Stellar allows transaction in many currencies, you must
-              // specify the asset type. The special "native" asset represents Lumens.
               asset: StellarSdk.Asset.native(),
               amount: amount,
             })
           )
-          // A memo allows you to add your own metadata to a transaction. It's
-          // optional and does not affect how Stellar treats the transaction.
           .addMemo(StellarSdk.Memo.text("Test Transaction"))
-          // Wait a maximum of three minutes for the transaction
           .setTimeout(180)
           .build();
-        // Sign the transaction to prove you are actually the person sending it.
         transaction.sign(sourceKeys);
-        // And finally, send it off to Stellar!
         return server.submitTransaction(transaction);
       })
       .then(function (result: any) {
@@ -91,20 +70,11 @@ const Account: React.FC<Props> = ({ setSubmit, wallet }: any) => {
       {loading ? (
         <p>Please Wait</p>
       ) : (
-        <div style={{paddingBottom: 40}}>
-          {/* display wallet balances */}
+        <div style={{ paddingBottom: 40 }}>
           <p>Your Balance: {wallet?.balances[0]?.balance} XLM</p>
           <p>Wallet Address: {wallet?.account_id}</p>
           <p>Send Funds</p>
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              setLoading(true);
-            
-              // unless an error, transactions always take few seconds / minutes to be completely done
-              setLoading(false);
-            }}
-          >
+          <form>
             <div>
               <label htmlFor="publicKey">Enter source account secret key</label>
               <br />
@@ -122,7 +92,9 @@ const Account: React.FC<Props> = ({ setSubmit, wallet }: any) => {
               />
             </div>
             <div>
-            <label htmlFor="publicKey">Enter destination source account id</label>
+              <label htmlFor="publicKey">
+                Enter destination source account id
+              </label>
               <br />
               <input
                 style={{
@@ -138,7 +110,7 @@ const Account: React.FC<Props> = ({ setSubmit, wallet }: any) => {
               />
             </div>
             <div>
-            <label htmlFor="publicKey">Enter amount to transfer</label>
+              <label htmlFor="publicKey">Enter amount to transfer</label>
               <br />
               <input
                 style={{
